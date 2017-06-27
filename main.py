8ead44f674ea8bba44c15420d13eb66c6c3e4d6b
ppreechakul-ring <ppreechakul@ring.com>
2017-06-26 22:21:55 -0700

    update

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, send_from_directory
 
 app = Flask(__name__)
 
@@ -8,6 +8,14 @@ app = Flask(__name__)
 def wedding():
     return render_template("home.html")
 
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
+
+@app.route('/image/<filename>')
+def image(filename=None):
+    return send_from_directory(directory=r'C:\Users\Phillip\Dropbox (Ring)\images',filename=filename)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5005)
